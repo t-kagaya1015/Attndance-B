b4c34608fb25d79adfdaaaf472e36a2aaa47410f
t-kagaya1015 <tnpp_kagaya1015@yahoo.co.jp>
2019-11-23 13:21:35 +0000

    6.2.3エラーメッセージを表示しよう

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   get '/signup', to: 'users#new'
 
- # ログイン機能
+  # ログイン機能
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
+
   resources :users
 end
