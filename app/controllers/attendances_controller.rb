d1cab6e5720ae8df6f0a14abc25e626b355a4a12
t-kagaya1015 <tnpp_kagaya1015@yahoo.co.jp>
2019-12-05 13:50:19 +0000

    11.3.1バリデーションを追加しよう

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -29,5 +29,11 @@ class AttendancesController < ApplicationController
   end
   
   def update_one_month
-  end 
+  end
+  
+  private
+  #　一か月分の勤怠情報を扱います。
+  def attendances_params
+    params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
+  end   
 end
