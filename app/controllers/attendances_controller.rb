dad74e90c6b82034fb9bdcbf0820a932ec5af75c
t-kagaya1015 <tnpp_kagaya1015@yahoo.co.jp>
2019-12-07 21:21:34 +0000

    11.3.2例外処理を含めた更新を追加

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -29,7 +29,18 @@ class AttendancesController < ApplicationController
   end
   
   def update_one_month
-  end
+   ActiveRecord::Base.transaction do #　トランザクションを開始します。 
+      attendances_params.each do |id, item|
+        attendance = Attendance.find(id)
+        attendance.update_attributes!(item)
+      end
+    end
+    flash[:success] = "１ヶ月分の勤怠情報を更新しました。"
+    redirect_to user_url(date: params[:date])
+  rescue ActiveRecord::RecordInvalid # トランザクションのようエラーの分岐です。
+  　flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+  　redirect_to attendances_edit_one_month_user_url(date: params[:date])
+  end  
   
   private
   
