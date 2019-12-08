dd9262ceea154cf0daa3169fc172c6ff0a505f29
t-kagaya1015 <tnpp_kagaya1015@yahoo.co.jp>
2019-12-08 13:54:13 +0000

    11.3.2例外処理を含めた更新を追加シフトAが出ない

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,5 +1,5 @@
 class AttendancesController < ApplicationController
-  before_action :set_user, only: :edit_one_month
+  before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_month]
   before_action :set_one_month, only: :edit_one_month
 
@@ -27,25 +27,24 @@ class AttendancesController < ApplicationController
 
   def edit_one_month
   end
-  
+
   def update_one_month
-   ActiveRecord::Base.transaction do #　トランザクションを開始します。 
+   ActiveRecord::Base.transaction do # トランザクションを開始します。
       attendances_params.each do |id, item|
         attendance = Attendance.find(id)
         attendance.update_attributes!(item)
       end
     end
-    flash[:success] = "１ヶ月分の勤怠情報を更新しました。"
+    flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
     redirect_to user_url(date: params[:date])
-  rescue ActiveRecord::RecordInvalid # トランザクションのようエラーの分岐です。
-  　flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
-  　redirect_to attendances_edit_one_month_user_url(date: params[:date])
-  end  
-  
-  private
-  
-  # 一か月分の勤怠情報を扱います。
-  def attendances_params
-    params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
+  rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
+    flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+    redirect_to attendances_edit_one_month_user_url(date: params[:date])
   end
+
+  private
+    # 1ヶ月分の勤怠情報を扱います。
+    def attendances_params
+      params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
+    end
 end
