db43b7758fcfc504a9e6f664c866b7c6eafd0361
EC2 Default User <ec2-user@ip-172-31-35-138.us-east-2.compute.internal>
2020-05-22 15:05:45 +0000

    No9

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,7 +1,9 @@
 class AttendancesController < ApplicationController
   before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_month]
+  before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
   before_action :set_one_month, only: :edit_one_month
+  
 
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
 
@@ -21,9 +23,10 @@ class AttendancesController < ApplicationController
       else
         flash[:danger] = UPDATE_ERROR_MSG
       end
-    end
-    redirect_to @user
+    end  
+   redirect_to @user
   end
+  
   def edit_one_month
   end
   
@@ -32,26 +35,37 @@ class AttendancesController < ApplicationController
       attendances_params.each do |id, item|
         attendance = Attendance.find(id)
         attendance.update_attributes!(item)
-      end
-    end
-    flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
-    redirect_to user_url(date: params[:date])
-  rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
-    flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
-    redirect_to attendances_edit_one_month_user_url(date: params[:date])
-  end 
-
+      if item[:stared_at].present? && item[:finished_at].blank?
+             flash[:danger] = "更新できませんでした。出社時間のみの更新はできません。"
+             redirect_to attendances_edit_one_month_user_url(params[:user_id])and return
+          if item[:stared_at].blank? && item[:finished_at].presnt?
+             flash[:danger] = "更新できませんでした。退社時刻のみの更新はできません。"
+             redirect_to attendances_edit_one_month_user_url(params[:user_id])and return
+          end 
+      end 
+      flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+      redirect_to user_url(date: params[:date]) and return
+     rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
+      flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+      redirect_to attendances_edit_one_month_user_url(date: params[:date]) and return
+     end
+   end 
+  end
+  
   private
     # 1ヶ月分の勤怠情報を扱います。
     def attendances_params
       params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
     end
-    # deforeフィルター
-    def admit_or_correct_user
+    
+  # beforeフィルター
+  
+  # 管理権限者、または現在ログインしているユーザーを許可します。
+    def admin_or_correct_user
       @user = User.find(params[:user_id]) if @user.blank?
-      unless current_user?(@user) || current_user.admit?
+      unless current_user?(@user) || current_user.admin?
         flash[:danger] = "編集権限がありません。"
-        redirect_to(_url)
-      end 
-    end     
+        redirect_to(root_url)
+      end
+    end 
 end
