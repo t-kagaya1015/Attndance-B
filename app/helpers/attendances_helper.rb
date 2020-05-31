81c55d19aabc6db42277ebe463849231a2a9ad45
EC2 Default User <ec2-user@ip-172-31-35-138.us-east-2.compute.internal>
2020-05-31 14:09:42 +0000

    土、日の色変更設定完了

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -13,7 +13,7 @@ module AttendancesHelper
   # 出勤時間と退勤時間を受け取り、在社時間を計算して返します。
   
   
-  def working_times(start)
+  def working_times(start, finish)
     format("%.2f", (((finish - start) / 60) /60))
   end
   
