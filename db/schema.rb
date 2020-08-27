ab08fd341a21333279465818e63f8dc275fbe9be
t.kagaya1015 <tnpp_kagaya1015@yahoo.co.jp>
2020-08-27 23:20:50 +0000

    Initialize repository

--- db/schema.rb
+++ db/schema.rb
@@ -33,8 +33,8 @@ ActiveRecord::Schema.define(version: 20191201091907) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2020-07-29 23:00:00"
-    t.datetime "work_time", default: "2020-07-29 22:30:00"
+    t.datetime "basic_time", default: "2020-08-27 23:00:00"
+    t.datetime "work_time", default: "2020-08-27 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
